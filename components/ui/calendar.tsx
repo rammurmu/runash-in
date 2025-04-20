6329257fe3ee9548a5e6ee95bdf58603e9d08b2b
Vaibhav Murmu <100757195+Vaibhavmurmu@users.noreply.github.com>
2025-04-20 06:28:24 +0530

    Update calendar.tsx

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
+import { ChevronLeft, ChevronRight, IconRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
@@ -60,10 +60,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        ChevronLeft: ({ className, ...props }) => (
+        IconLeft: ({ className, ...props }) => (
           <ChevronLeft className={cn("size-4", className)} {...props} />
         ),
-        ChevronRight: ({ className, ...props }) => (
+        IconRight: ({ className, ...props }) => (
           <ChevronRight className={cn("size-4", className)} {...props} />
         ),
       }}
